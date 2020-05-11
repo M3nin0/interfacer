efc5ca74758a8f3198f69951aa08864da716ff78
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-05-11 17:55:40 -0300

    Organização do repositório

--- interfacer/descdist.py
+++ interfacer/descdist.py
@@ -4,7 +4,10 @@ from rpy2.robjects import pandas2ri
 from rpy2.robjects.packages import importr 
 
 pandas2ri.activate()
-fitdistrplus = importr('fitdistrplus')
+try:
+    fitdistrplus = importr('fitdistrplus')
+except:
+    raise ModuleNotFoundError("É necessário instalar o pacote fitdistrplus no ambiente R")
 
 def descdist(data: pd.Series, **kwargs):
     rdf = pandas2ri.py2rpy(data)
